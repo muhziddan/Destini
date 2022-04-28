84262cdbc49aed83ef6667bf44121baf98e70c74
muhammad ziddan hidayatullah <ziddan.mz@gmail.com>
2022-04-28 14:37:57 +0700

    add model, update ui

--- Destini/Controller/ViewController.swift
+++ Destini/Controller/ViewController.swift
@@ -8,11 +8,34 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var choiceOneButton: UIButton!
+    @IBOutlet weak var choiceTwoButton: UIButton!
+    @IBOutlet weak var storyLabel: UILabel!
+    
+    var story = Story()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        updateUI()
     }
-
-
+    
+    @IBAction func buttonPressed(_ sender: UIButton) {
+        
+        guard let choice = sender.titleLabel?.text else { return }
+        
+        story.nextStories(userChoice: choice)
+        
+        updateUI()
+    }
+    
+    func updateUI() {
+        
+        let currentstories = story.storyList[story.choiceDestination]
+        
+        storyLabel.text = currentstories.title
+        choiceOneButton.setTitle(currentstories.choice1, for: UIControl.State.normal)
+        choiceTwoButton.setTitle(currentstories.choice2, for: UIControl.State.normal)
+    }
 }
